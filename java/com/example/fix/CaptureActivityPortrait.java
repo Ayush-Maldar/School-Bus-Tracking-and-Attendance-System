72db1172c845d5bab24f73a39f1fc24d2c4ba9a5
Ayush-Maldar <ayush.maldar.10@gmail.com>
2025-07-07 10:43:41 +0530

    Delete java directory

--- java/com/example/fix/CaptureActivityPortrait.java
+++
@@ -1,8 +0,0 @@
-package com.example.fix; // Ensure this package name matches your project structure
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-
-public class CaptureActivityPortrait extends CaptureActivity {
-
-}
